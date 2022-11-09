c8a5acb6119cfc2f747a5776e7a3e6a28c251c1e
Sharkzuria <baumabulondo@gmail.com>
2022-11-09 16:26:27 +0300

    feat: Add Non Closable alert if permission is denied

--- FoodTuck/View/Home.swift
+++ FoodTuck/View/Home.swift
@@ -11,52 +11,65 @@ struct Home: View {
     @StateObject var HomeModel = HomeViewModel()
     
     var body: some View {
-        VStack(spacing: 10){
-            HStack(spacing: 15){
+        ZStack {
+            VStack(spacing: 10){
+                HStack(spacing: 15){
 
-                Button {
-                    
-                } label: {
-                    Image(systemName: "line.3.horizontal")
-                        .font(.title)
-                        .foregroundColor(Color("PrimaryColor"))
-                }
-
-                Text("Deliver to")
-                    .foregroundColor(.black)
-                
-                Text("Apple")
-                    .font(.caption)
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color("PrimaryColor"))
-                
-                Spacer(minLength: 0)
-            }
-            .padding([.horizontal, .top])
-             
-            Divider()
-            
-            HStack(spacing: 15){
-                TextField("Search", text: $HomeModel.search)
-                
-                if HomeModel.search != "" {
                     Button {
                         
                     } label: {
-                        Image(systemName: "magnifyingglass")
-                            .font(.title2)
-                            .foregroundColor(.gray)
+                        Image(systemName: "line.3.horizontal")
+                            .font(.title)
+                            .foregroundColor(Color("PrimaryColor"))
                     }
-                    .animation(.easeIn, value: 0.3)
+
+                    Text(HomeModel.userLocation == nil ?  "Locating..." : "Deliver to")
+                        .foregroundColor(.black)
+                    
+                    Text(HomeModel.userAddress)
+                        .font(.caption)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color("PrimaryColor"))
+                    
+                    Spacer(minLength: 0)
                 }
+                .padding([.horizontal, .top])
+                 
+                Divider()
+                
+                HStack(spacing: 15){
+                    TextField("Search", text: $HomeModel.search)
+                    
+                    if HomeModel.search != "" {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "magnifyingglass")
+                                .font(.title2)
+                                .foregroundColor(.gray)
+                        }
+                        .animation(.easeIn, value: 0.3)
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top, 10)
+                
+                Divider()
+                
+                Spacer()
+                
             }
-            .padding(.horizontal)
-            .padding(.top, 10)
             
-            Divider()
-            
-            Spacer()
-            
+            // Non Closable alert if permission is denied
+            if HomeModel.noLocation {
+                Text("Please enable location access in settings to further move on!!!")
+                    .foregroundColor(.black)
+                    .frame(width: UIScreen.main.bounds.width - 100, height: 120)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .frame(maxWidth: .infinity,maxHeight: .infinity)
+                    .background(Color.black.opacity(0.3).ignoresSafeArea())
+            }
         }
     }
         
--- FoodTuck/ViewModel/HomeViewModel.swift
+++ FoodTuck/ViewModel/HomeViewModel.swift
@@ -15,20 +15,58 @@ class HomeViewModel: NSObject, ObservableObject, CLLocationManagerDelegate{
     @Published var locationManager = CLLocationManager()
     @Published var search = ""
     
+    // Location Details
+    @Published var userLocation: CLLocation!
+    @Published var userAddress = ""
+    @Published var noLocation = false
+    
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         // Checking Location access
         
         switch manager.authorizationStatus {
         case .authorizedWhenInUse:
             print("Authorized")
+            self.noLocation = false
+            manager.requestLocation()
         case .denied:
             print("denied")
+            self.noLocation = true
         default:
             print("Unknown")
+            self.noLocation = false
+            // Direct Call
+            locationManager.requestAlwaysAuthorization()
+            // Modifying Info.plist
+            
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error.localizedDescription)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+         // Reading user location and extracting Details
+        self.userLocation = locations.last
+        self.extractLocation()
+    }
+    
+    func extractLocation(){
+        CLGeocoder().reverseGeocodeLocation(self.userLocation) { (res, err) in
+            
+            
+            guard let safeData = res else {return}
+            
+            var address = ""
+            
+            // getting area and location name
+            
+            address += safeData.first?.name ?? ""
+            address += ", "
+            address += safeData.first?.locality ?? ""
+            
+            self.userAddress = address
+            
+        }
     }
 }
