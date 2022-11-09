29efa60bde12066ab6d0752c5507706a176b0fe1
Sharkzuria <baumabulondo@gmail.com>
2022-11-09 14:08:08 +0300

    style: Add Home View

---
+++ FoodTuck/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  FoodTuck
+//
+//  Created by Le Bon B' Bauma on 09/11/2022.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
