0b08e20e335070df0acc6f1db6b8a9b0e6869135
Sharkzuria <baumabulondo@gmail.com>
2022-11-09 14:05:10 +0300

    Initial Commit

---
+++ FoodTuck/FoodTuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTuckApp.swift
+//  FoodTuck
+//
+//  Created by Le Bon B' Bauma on 09/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodTuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
