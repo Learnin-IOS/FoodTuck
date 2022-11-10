3084dd81cd38f76ccd88b87b40546a38c3d8e7bf
Sharkzuria <baumabulondo@gmail.com>
2022-11-10 15:32:54 +0300

    build: Add Item Data Model

---
+++ FoodTuck/Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FoodTuck
+//
+//  Created by Le Bon B' Bauma on 10/11/2022.
+//
+
+import SwiftUI
+
+struct Item: Identifiable{
+    
+    var id: String
+    var item_name: String
+    var item_details: String
+    var item_cost: NSNumber
+    var item_rating: String
+    var item_image: String
+}
